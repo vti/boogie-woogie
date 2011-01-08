6cce2a31943514b80f52e7a66e19e673292620fd
vti <viacheslav.t@gmail.com>
2011-01-08 20:15:49 +0200

    Fixup template prefix

--- lib/BoogieWoogie/View/Base.pm
+++ lib/BoogieWoogie/View/Base.pm
@@ -6,4 +6,16 @@ has 'app' => {is_weak => 1};
 has 'format' => 'html';
 has 'templates_path' => sub { $_[0]->app->home->reldir('views') };
 
+sub _class_to_template {
+    my $self = shift;
+
+    my $prefix = ref $self->app;
+
+    my $template = $self->SUPER::_class_to_template;
+
+    $template =~ s/^$prefix-//i;
+
+    return $template;
+}
+
 1;
