8b8354372f8d1d8e5e99af33c24b2eaf22760b12
vti <viacheslav.t@gmail.com>
2011-01-21 13:58:46 +0200

    Cleanup

--- lib/BoogieWoogie/View.pm
+++ lib/BoogieWoogie/View.pm
@@ -1,10 +1,28 @@
 package BoogieWoogie::View;
+use Boose 'Text::Caml';
 
-use strict;
-use warnings;
+use BoogieWoogie::Util 'decamelize';
 
-use base 'Boose';
+has [qw/app req/] => {is_weak => 1};
+has 'format' => 'html';
+has 'templates_path' => sub { $_[0]->app->home->reldir('views') };
 
-use BoogieWoogie::View::Base;
+sub url_for {
+    my $self = shift;
+
+    return $self->app->dispatcher->url_for($self->req, @_);
+}
+
+sub _class_to_template {
+    my $self = shift;
+
+    my $prefix = decamelize(ref $self->app);
+
+    my $template = $self->SUPER::_class_to_template;
+
+    $template =~ s/^$prefix-//i;
+
+    return $template;
+}
 
 1;
